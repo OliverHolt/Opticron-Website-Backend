2690909c5f86b2afaf1d54cdda8a11ae2454f0a2
OliverHolt <oliverdholt@gmail.com>
2022-12-09 22:29:49 +0000

    refactor GET /api

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,10 @@
 const fs = require("fs");
+const path = require("path");
 
 exports.getEndpoints = (req, res, next) => {
-  fs.readFile(
-    "/home/oliverholt/northcoders/backend/be-nc-news/endpoints.json",
-    (err, data) => {
-      if (err) throw err;
+  fs.readFile(path.join(__dirname, "../endpoints.json"), (err, data) => {
+    if (err) throw err;
 
-      res.status(200).send(JSON.parse(data));
-    }
-  );
+    res.status(200).send(JSON.parse(data));
+  });
 };
